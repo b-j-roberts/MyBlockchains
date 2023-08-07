9d2b6410570a9600f99c611ff40b4f4476cf9565
Brandon Roberts <brandonjroberts22@gmail.com>
2023-08-06 22:52:48 -0700

    Sequencer github actions 1

--- naive-blockchain/naive-cryptocurrency-l2/scripts/start-sequencer-retry-github-actions.sh
+++ naive-blockchain/naive-cryptocurrency-l2/scripts/start-sequencer-retry-github-actions.sh
@@ -6,10 +6,11 @@ WORK_DIR="${SCRIPT_DIR}/.."
 while true; do
   cd ${WORK_DIR} && make run-sequencer-github-actions
   sleep 20
-  if cat ${L2_SEQUENCER_LOGS} | grep Commit\ new\ sealing\ work | grep -q number=5; then
+  if cat ${OUTPUT_FILE} | grep Commit\ new\ sealing\ work | grep -q number=5; then
     break
   fi
   echo "Sequencer failed to start, restarting..."
+  cat $OUTPUT_FILE
   ps aux | grep scripts/../build/sequencer | awk '{print $2}' | xargs kill
   sleep 5
 done
